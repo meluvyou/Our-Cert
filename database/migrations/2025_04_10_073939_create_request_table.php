71ee7f91b1fcf55b74192cd2f3ddc8ff078c68bf
cherray <navarrocherry17@gmail.com>
2025-05-07 19:37:20 +0800

    update

--- database/migrations/2025_04_10_073939_create_request_table.php
+++ database/migrations/2025_04_10_073939_create_request_table.php
@@ -15,12 +15,20 @@ return new class extends Migration
 
             $table->bigIncrements('id');
             $table->string('first_name,',15);
-            $table->string('last_name',15);
             $table->string('middle_name',15);
-            $table->string('email',20);
-            $table->string('phone',11);
-            $table->string('address',25);
+            $table->string('last_name',15);
+            $table->string('suffix',15);
+            $table->string('email',25);
+            $table->string('purok',15);
+            $table->string('barangay',25);
             $table->string('city',25);
+            $table->string('province',25);
+            $table->string('date_of_birth',15);
+            $table->string('contact_number',11);
+            $table->string('request_type',20);
+            $table->integer('civilstatus')->default(0);
+            $table->integer('status')->default(0);
+            $table->string('request_purpose',50);
             $table->timestamps();
         });
     }
