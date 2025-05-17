60ec67527609de7f9e6f66044e679dda4876175c
cherray <navarrocherry17@gmail.com>
2025-05-17 18:54:40 +0800

    Update Index.vue

--- resources/js/pages/User/Index.vue
+++ resources/js/pages/User/Index.vue
@@ -15,7 +15,7 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-defineProps({users:Array});
+defineProps({ users: Array });
 
 </script>
 
@@ -24,7 +24,7 @@ defineProps({users:Array});
     <Head title="Users" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-       <!-- create a table list of user make it compact -->
+        <!-- create a table list of user make it compact -->
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="overflow-hidden rounded-lg border bg-card shadow-md dark:border-slate-700 dark:bg-slate-800">
                 <table class="w-full table-auto text-left text-sm text-gray-500 dark:text-gray-400">
@@ -32,24 +32,32 @@ defineProps({users:Array});
                         <tr>
                             <th scope="col" class="px-6 py-3">Name</th>
                             <th scope="col" class="px-6 py-3">Email</th>
+                            <th scope="col" class="px-6 py-3">Status</th>
                             <th scope="col" class="px-6 py-3">Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         <!-- Add user rows here -->
-                         <tr v-for="user in users" :key="user.id" class="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
+                        <tr v-for="user in users" :key="user.id"
+                            class="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
                             <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">{{ user.name }}</td>
                             <td class="px-6 py-4">{{ user.email }}</td>
                             <td class="px-6 py-4">
-                                <button class="text-blue-600 hover:text-blue-900 dark:text-blue-500 dark:hover:text-blue-400">Edit</button>
+                                <span v-if="user.status === 0" class="text-green-600">Active</span>
+                                <span v-else class="text-red-600">Inactive</span>
+                            </td>
+                            <td class="px-6 py-4">
+                                <button
+                                    class="text-blue-600 hover:text-blue-900 dark:text-blue-500 dark:hover:text-blue-400">Edit</button>
                                 &nbsp;|&nbsp;
-                                <button class="text-red-600 hover:text-red-900 dark:text-red-500 dark:hover:text-red-400">Delete</button>
+                                <button
+                                    class="text-red-600 hover:text-red-900 dark:text-red-500 dark:hover:text-red-400">Delete</button>
                             </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
         </div>
-        
+
     </AppLayout>
 </template>
