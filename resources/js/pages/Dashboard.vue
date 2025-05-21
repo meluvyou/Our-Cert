ea248c0475556591e22c023bce0a32b38c216239
cherray <141354843+meluvyou@users.noreply.github.com>
2025-05-21 22:34:29 +0800

    update dashboard

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -20,48 +20,39 @@ const breadcrumbs: BreadcrumbItem[] = [
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border bg-blue-600 dark:bg-blue-700 flex items-center justify-between p-6 shadow">
+                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border bg-white-600 dark:bg-white-700 flex items-center justify-between p-6 shadow">
                     <div>
-                        <h2 class="text-lg font-semibold text-white mb-2">Total Requests</h2>
-                        <p class="text-3xl font-bold text-white">128</p>
+                        <h2 class="text-lg font-semibold text-black mb-2">Total Requests</h2>
+                        <p class="text-4xl font-bold text-black">128</p>
                         <div class="mt-4">
-                            <span
-                                class="inline-block rounded bg-blue-100/80 px-2 py-1 text-xs font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-200">+12%
-                                this month</span>
                         </div>
                     </div>
-                    <img src="/images/request-card2.svg" alt="Requests" class="h-24 w-24 object-contain ml-4" />
+                    <img src="/images/request-approved.svg" alt="Requests" class="h-12 w-20 object-contain ml-4" />
                 </div>
                 <div
                     class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <div
-                        class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border bg-green-600 dark:bg-green-700 flex items-center justify-between p-6 shadow">
+                        class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border bg-white-600 dark:bg-white-700 flex items-center justify-between p-6 shadow">
                         <div>
-                            <h2 class="text-lg font-semibold text-white mb-2">Total Quest</h2>
-                            <p class="text-3xl font-bold text-white">128</p>
+                            <h2 class="text-lg font-semibold text-black mb-2">Pending Request</h2>
+                            <p class="text-4xl font-bold text-red">2</p>
                             <div class="mt-4">
-                                <span
-                                    class="inline-block rounded bg-blue-100/80 px-2 py-1 text-xs font-medium text-green-800 dark:bg-green-900 dark:text-blue-200">+12%
-                                    this month</span>
                             </div>
                         </div>
-                        <img src="/images/request-card.svg" alt="Requests" class="h-24 w-24 object-contain ml-4" />
+                        <img src="/images/pending.svg" alt="Requests" class="h-20 w-18 object-contain ml-4" />
                     </div>
                 </div>
                 <div
                     class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-            <div
-                        class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border bg-green-600 dark:bg-green-700 flex items-center justify-between p-6 shadow">
+                    <div
+                        class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border bg-white-600 dark:bg-white-700 flex items-center justify-between p-6 shadow">
                         <div>
-                            <h2 class="text-lg font-semibold text-white mb-2">Total Admin</h2>
-                            <p class="text-3xl font-bold text-white">128</p>
+                            <h2 class="text-lg font-semibold text-black mb-2">Total Admin</h2>
+                            <p class="text-4xl font-bold text-black">4</p>
                             <div class="mt-4">
-                                <span
-                                    class="inline-block rounded bg-blue-100/80 px-2 py-1 text-xs font-medium text-green-800 dark:bg-green-900 dark:text-blue-200">+12%
-                                    this month</span>
                             </div>
                         </div>
-                        <img src="/images/request-card.svg" alt="Requests" class="h-24 w-24 object-contain ml-4" />
+                        <img src="/images/admin.svg" alt="Requests" class="h-14 w-20 object-contain ml-2" />
                     </div>
                 </div>
             </div>
