91a48935c0f6c349bf6549436249e06de9a41334
cherray <150115404+Drelmeca@users.noreply.github.com>
2025-05-23 09:26:22 +0800

    Revert "Logo-Edited"
    This reverts commit ee303fb9d577a0a96540804e9bb8a0048ed69f0d.

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -7,6 +7,6 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
         <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate font-semibold leading-none">Barangay Certificate Online Request System</span>
+        <span class="mb-0.5 truncate font-semibold leading-none">Our-Certt</span>
     </div>
 </template>
